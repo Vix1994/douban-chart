1ecd6a250cd6fa02db2e47c3281669cbf255ed4f
m_uchi <m_uchi@outlook.com>
2020-09-09 23:24:07 +0800

    Add write logs to local file

--- GroupData/Common/Log.cs
+++ GroupData/Common/Log.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using DataCatch.Douban.Base;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace DataCatch.Common
 {
-
     public static class Log
     {
         /// <summary>
@@ -11,15 +13,34 @@ namespace DataCatch.Common
         /// <param name="log"></param>
         private static void Output(string log, string logLevel, ConsoleColor color)
         {
+            var output = $"[{DateTime.Now}] [{logLevel}] {log}";
             Console.ForegroundColor = color;
-            Console.WriteLine($"[{DateTime.Now}] [{logLevel}] {log}");
+            Console.WriteLine(output);
         }
 
         public static void Info(string log) =>
-            Output(log, "Info", ConsoleColor.White);
+            Output(log, "Info", ConsoleColor.Green);
         public static void Debug(string log) =>
             Output(log, "Debug", ConsoleColor.Yellow);
         public static void Error(string log) =>
             Output(log, "Error", ConsoleColor.Red);
+    }
+
+    public class WriteLog : IProcess
+    {
+        /// <summary>
+        /// 输出日志到目录
+        /// </summary>
+        /// <returns></returns>
+        public async Task Start()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                "Log",
+                DateTime.Now.ToString("yyyy-MM-dd"));
+
+            Directory.CreateDirectory(path);
+
+            // write current console logs
+        }
     }
 }
